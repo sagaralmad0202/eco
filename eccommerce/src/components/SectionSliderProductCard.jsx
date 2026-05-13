51c8f4b7dc7c215e820fccffa5d8b37cff1e1d44
sagaralmad2025-dev <sagaralmad2025@gmail.com>
2026-05-13 12:42:01 +0530

    fixed the product miss match in the sectionsliderprodctcard

--- eccommerce/src/components/SectionSliderProductCard.jsx
+++ eccommerce/src/components/SectionSliderProductCard.jsx
@@ -7,28 +7,11 @@ import p7Asset from "../assets/p7.webp";
 import p8Asset from "../assets/p8.webp";
 
 const DEMO_DATA = [
-  { id: 1, name: "Linen Blazer", desc: "Beige", price: "95.00", rating: 4.4, reviews: 60, image: p5Asset, colors: ['rgb(245, 245, 220)', 'rgb(0, 0, 128)', 'rgb(128, 128, 0)'] },
-
-
-
-  { id: 2, name: "Cashmere Sweater", desc: "Cream", price: "150.00", rating: 4.8, reviews: 75, image: p4Asset, colors: ['#3b474e', '#fc9faf', '#811428'] },
-
-
-
-
-
-
-
-
-  { id: 6, name: "Velvet Skirt", desc: "Wine Red", price: "55.00", rating: 4.2, reviews: 45, image: p8Asset },
-
-
-
-  { id: 7, name: "Wool Trench Coat", desc: "Camel", price: "180.00", rating: 4.6, reviews: 80, image: p6Asset },
-
-
-
-  { id: 8, name: "Cotton Shirt", desc: "White", price: "45.00", rating: 4.1, reviews: 110, image: p7Asset },
+  { id: 2, name: "Cashmere Sweater", desc: "Cream", price: "150.00", rating: 4.8, reviews: 75, image: p4Asset, colors: ['#3b474e', '#fc9faf', '#811428'], badge: false },
+  { id: 1, name: "Linen Blazer", desc: "Beige", price: "95.00", rating: 4.4, reviews: 60, image: p5Asset, colors: ['#f5f5dc', '#000080', '#6b8e23'] },
+  { id: 6, name: "Velvet Skirt", desc: "Wine Red", price: "55.00", rating: 4.2, reviews: 45, image: p6Asset, colors: ['#1e1b4b', '#7f1d1d', '#4ade80'], badge: false },
+  { id: 7, name: "Wool Trench Coat", desc: "Camel", price: "180.00", rating: 4.6, reviews: 80, image: p7Asset, colors: ['#c2a27b', '#1c1917', '#78716c'] },
+  { id: 8, name: "Cotton Shirt", desc: "White", price: "45.00", rating: 4.1, reviews: 110, image: p8Asset, colors: ['#fbcfe8', '#bae6fd', '#fecdd3'], badge: false },
 ];
 
 const SectionSliderProductCard = ({ className = "", data = DEMO_DATA, onQuickView }) => {
